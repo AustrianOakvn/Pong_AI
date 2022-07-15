cc0b4726fab206b2f169071fe76e64fb3da38020
AustrianOakvn <austrian.oak.vn@gmail.com>
2022-07-15 23:40:21 +0700

    add Paddle class

--- src/Ball.js
+++ src/Ball.js
@@ -1,5 +1,5 @@
 const INITIAL_VELOCITY = 0.025
-
+const VELOCITY_INCREASE = 0.00001
 export default class Ball {
     constructor(ballElem){
         this.ballElem = ballElem
@@ -22,12 +22,16 @@ export default class Ball {
         this.ballElem.style.setProperty("--y", value)
     }
 
+    rect(){
+        return this.ballElem.getBoundingClientRect()
+    }
+
     reset(){
         this.x = 50
         this.y = 50
         // Need to be unit vector 
-        this.direction = {x: 0.75, y: 0.5}
-        while (Math.abs(this.direction.x) <= .2 || Math.abs(this.direction.x) >= .9){
+        this.direction = {x: 0}
+        while (Math.abs(this.direction.x) <= 0.2 || Math.abs(this.direction.x) >= 0.9){
             const heading = randomNumberBetween(0, 2 * Math.PI)
             this.direction = {x: Math.cos(heading), y: Math.sin(heading)}
         }
@@ -38,11 +42,21 @@ export default class Ball {
     update(delta){
         this.x += this.direction.x * this.velocity * delta
         this.y += this.direction.y * this.velocity * delta
+        this.velocity *= VELOCITY_INCREASE * delta
+        const rect = this.rect()
+
+        if (rect.bottom >= window.innerHeight || rect.top <= 0){
+            this.direction.y *= -1
+        }
+        if (rect.right >= window.innerWidth || rect.left <= 0){
+            this.direction.x *= -1
+        }
+        
 
     }
 }
 
 
 function randomNumberBetween(min, max){
-    return Math.random()*(max-min) + min
+    return Math.random()* (max - min) + min
 }
