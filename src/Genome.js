7a73b9ea4957c25890631c3c7e4a50c1be50a6ec
AustrianOakvn <austrian.oak.vn@gmail.com>
2022-07-19 23:29:07 +0700

    add function in class genome.js

--- src/Genome.js
+++ src/Genome.js
@@ -30,4 +30,74 @@ class Genome {
         this.nextNode++;
         this.node[this.biasNode].layer = 0;
     }
+
+
+    fullyConnect(innovationHistory){
+        for (var i=0; i<this.inputs; i++){
+            for (var j=0; j<this.outputs; j++){
+                var connectionInnovationNumber = this.getInnovationNumber(innovationHistory, this.nodes[i], this.nodes[this.nodes.length -j -2]);
+            }
+        }
+    }
+
+    getNode(){
+        
+    }
+
+    connectNodes(){
+
+    }
+
+    feedForward(){
+
+    }
+
+    generateNetwork(){
+
+    }
+
+
+    addNode(innovationHistory){
+
+    }
+
+    addConnection(innovationHistory){
+
+    }
+
+    randomConnectionNodesAreShit(r1, r2){
+
+    }
+
+    getInnovationNumber(innovationHistory, from, to){
+
+    }
+
+    fullyConnected(){
+
+    }
+
+    mutate(){
+
+    }
+
+    crossover(){
+
+    }
+
+    matchingGene(){
+
+    }
+
+    printGenome(){
+
+    }
+
+    clone(){
+
+    }
+
+    drawGenome(){
+
+    }
 }
